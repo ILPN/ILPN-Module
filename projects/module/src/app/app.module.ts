b457901ea98cffb0833fe8c38d0c29c5aaa2c99e
Jakub Kovář <6195502+minop@users.noreply.github.com>
2022-04-29 18:50:26 +0200

    attempt at linking the lib

--- projects/module/src/app/app.module.ts
+++ projects/module/src/app/app.module.ts
@@ -1,13 +1,15 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
+import {IlpnComponentsModule} from 'ilpn-components';
 
 @NgModule({
     declarations: [
         AppComponent
     ],
     imports: [
-        BrowserModule
+        BrowserModule,
+        IlpnComponentsModule
     ],
     providers: [],
     bootstrap: [AppComponent]
