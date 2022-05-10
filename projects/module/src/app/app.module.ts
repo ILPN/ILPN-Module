69e6649dc14821822a9e785db5c3aa0f3c8c5b80
Jakub Kovář <6195502+minop@users.noreply.github.com>
2022-05-10 14:34:07 +0200

    updated module template

--- projects/module/src/app/app.module.ts
+++ projects/module/src/app/app.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {IlpnComponentsModule} from 'ilpn-components';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 @NgModule({
     declarations: [
@@ -9,7 +10,8 @@ import {IlpnComponentsModule} from 'ilpn-components';
     ],
     imports: [
         BrowserModule,
-        IlpnComponentsModule
+        IlpnComponentsModule,
+        FlexLayoutModule,
     ],
     providers: [],
     bootstrap: [AppComponent]
