9ae26eb123389dedd03660fc020d31fd81b11b7b
Jakub Kovář <6195502+minop@users.noreply.github.com>
2022-05-11 13:16:13 +0200

    base href

--- projects/module/src/app/app.component.ts
+++ projects/module/src/app/app.component.ts
@@ -1,9 +1,14 @@
 import {Component} from '@angular/core';
+import {APP_BASE_HREF} from '@angular/common';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss']
+    styleUrls: ['./app.component.scss'],
+    providers: [
+        // TODO base href
+        {provide: APP_BASE_HREF, useValue: '/ilovepetrinets/'}
+    ]
 })
 export class AppComponent {
 }
