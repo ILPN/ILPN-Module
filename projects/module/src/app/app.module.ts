a0c7b99172d6cd23cebe8ff2883bba478a4b9b0a
Jakub Kovář <6195502+minop@users.noreply.github.com>
2025-01-27 15:02:20 +0100

    components v1.4

--- projects/module/src/app/app.module.ts
+++ projects/module/src/app/app.module.ts
@@ -1,7 +1,12 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-import {AppComponent} from './app.component';
-import {IlpnComponentsModule} from 'ilpn-components';
+import {AppComponent} from './app.component';import {
+    IlpnAlgorithmsModule,
+    IlpnComponentsModule,
+    PetriNetLayoutManagerFactoryService,
+    PnDisplayModule,
+    SpringEmbedderLayoutManagerFactoryService
+} from 'ilpn-components';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import {ReactiveFormsModule} from '@angular/forms';
 import {MatSlideToggleModule} from '@angular/material/slide-toggle';
@@ -17,6 +22,10 @@ import {AppRoutingModule} from './app-routing.module';
         AppRoutingModule,
         BrowserModule,
         IlpnComponentsModule,
+        PnDisplayModule,
+        IlpnAlgorithmsModule.withDebugConfig({
+            logRegions: true
+        }),
         FlexLayoutModule,
         ReactiveFormsModule,
         MatSlideToggleModule,
@@ -27,6 +36,10 @@ import {AppRoutingModule} from './app-routing.module';
             provide: APP_BASE_HREF,
             useFactory: (s: PlatformLocation) => s.getBaseHrefFromDOM(),
             deps: [PlatformLocation]
+        },
+        {
+            provide: PetriNetLayoutManagerFactoryService,
+            useExisting: SpringEmbedderLayoutManagerFactoryService,
         }
     ],
     bootstrap: [AppComponent]
